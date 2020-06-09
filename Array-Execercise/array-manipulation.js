e2d9993f7c707a375cd5c4cedb32f101e3ab87e7
LalitKushwah <meghamehrotra123@gmail.com>
2020-06-09 23:38:52 +0530

    update array manipulation logic

--- Array-Execercise/array-manipulation.js
+++ Array-Execercise/array-manipulation.js
@@ -3,25 +3,27 @@
 // Complete the arrayManipulation function below.
 function arrayManipulation(n, queries) {
     const arr = [];
-    for(let i=0;i<n;i++) {
+    for(let i=0;i<n+2;i++) {
         arr[i] = 0;
     }
-    let maxElement = 0;
     for(let j=0; j<queries.length; j++) {
-        let max = 0;
-        for(let i=0; i<arr.length;i++) {            
-            if (i >= (queries[j][0]-1) && i <= (queries[j][1]-1)) {
-                arr[i] = arr[i] + queries[j][2];
-            }
-            max = arr[i] < max ? max : arr[i];
-        };
+        // let max = 0;
+        const a = queries[j][0];
+        const b = queries[j][1];
+        const k = queries[j][2];
+        arr[a] += k;
+        arr[b+1] -= k;
         console.log(arr);
-        maxElement = max;
     };
 
-    console.log(maxElement)
     // find max element
-    return maxElement;
+    let max = 0;
+    let sum = 0;
+    for (let i=0; i<arr.length;i++) {
+        sum = sum + arr[i];
+        max = sum > max ? sum : max;
+    }
+    return max;
 }
 
-arrayManipulation(5, [[1,2,100],[2,5,100],[3,4,100]]);
+console.log(arrayManipulation(5, [[1,2,100],[2,5,100],[3,4,100]]));
